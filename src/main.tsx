22c973496635016ec039df0fec97d9b4919908ab
Susie Douang <soutsada.d@gmail.com>
2024-09-15 14:47:40 -0500

    create login/create account capability w/ AWS amplify

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Amplify } from 'aws-amplify'
+import config from './aws-exports'
+
+Amplify.configure(config);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
